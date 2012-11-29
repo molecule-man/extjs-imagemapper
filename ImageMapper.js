e101a65def83ac70e29067819bf3a991bec2ca02
Andrey Berezhinsky <a.berezh@gmail.com>
2012-11-29 16:40:08 +0200

    made selections to change their geometry when image changes. Added selection creation drag and resize features

--- ImageMapper.js
+++ ImageMapper.js
@@ -2,14 +2,10 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
     extend: 'Ext.view.View',
     requires: [
         //'Ext.LoadMask',
+        'Ext.resizer.Resizer',
         'Ext.dd.DragTracker'
     ],
     alias: 'widget.imagemapper',
-
-
-    mixins: {
-        bindable: 'Ext.util.Bindable'
-    },
 
 
     /**
@@ -43,7 +39,7 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
 
 
     minZoom: 0.2,
-    maxZoom: 3,
+    maxZoom: 4,
 
     style: {background: 'black'},
 
@@ -56,19 +52,10 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
 
     initComponent: function() {
         var me = this,
-            memberFn = {
-                disableFormats: true,
-                getCoordinate: Ext.bind(me.getNormalizedCoordinate, me),
-                getSize: Ext.bind(me.getNormalizedSize, me),
-            },
 
             // css style to apply to selection (mapping) node
             selectorStyle = [
                 'position:  absolute;',
-                'left:      {[this.getCoordinate(values.x)]}px;',
-                'top:       {[this.getCoordinate(values.y)]}px;',
-                'width:     {[this.getSize(values.w)]}px;',
-                'height:    {[this.getSize(values.h)]}px;',
                 'border:    1px solid #f00;', 
                 'z-index:   2;'
             ].join(''),
@@ -116,7 +103,7 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
         ].join('');
 
         me.itemSelector = 'div.'+me.selectorClass;
-        me.tpl = new Ext.XTemplate(itemTpl, memberFn);
+        me.tpl = new Ext.XTemplate(itemTpl);
 
         me.style = {
             position: 'relative',
@@ -144,25 +131,30 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
              * @param object oldSize. old size in format:
              *      {width: <oldWidth>, height: <oldHeight>} 
              */
-            'imgsizechanged'
+            'imgsizechanged',
+
+            /**
+             * @event selectioncreate
+             * Fires when new selection created
+             * @param Ext.data.Model selection record
+             */
+            'selectioncreate',
+
+            /**
+             * @event selectionupdate
+             * Fires when selection updated
+             * @param Ext.data.Model selection record
+             */
+            'selectionupdate'
         );
 
         me.geometry = {};
+        me.resizers = [];
 
         me.callParent(arguments);
 
         me.on('imgposchanged', me.onPosChange, me);
         me.on('imgsizechanged', me.onSizeChange, me);
-    },
-
-
-    getNormalizedCoordinate: function(coordinate) {
-        return coordinate;
-    },
-
-
-    getNormalizedSize: function(size) {
-        return size;
     },
 
 
@@ -190,7 +182,58 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
             onDrag: Ext.bind(me.onImgDrag, me)
         });
 
+        me.elDragger = Ext.create('Ext.dd.DragTracker', {
+            el: me.el,
+            onBeforeStart: Ext.bind(me.onBeforeElDrag, me),
+            onStart: Ext.bind(me.onStartElDrag, me),
+            onDrag: Ext.bind(me.onElDrag, me),
+            onEnd: Ext.bind(me.onEndElDrag, me),
+        });
+
         me.callParent(arguments);
+    },
+
+
+    refresh: function() {
+        this.callParent(arguments);
+        this.adjustSelections();
+        this.hookupResizers();
+    },
+
+
+    onAdd: function(ds, records) {
+        this.callParent(arguments);
+        this.adjustSelections(records);
+        this.hookupResizers();
+    },
+
+
+    onUpdate: function() {
+        this.callParent();
+    },
+
+
+    hookupResizers: function() {
+        var me = this;
+
+        me.clearResizers();
+
+        var nodes = me.all.elements,
+            resizer,
+            node;
+
+        for (var i = 0, total = nodes.length; i < total; i++) {
+            node = nodes[i];
+            var resizer = Ext.create('Ext.resizer.Resizer', {
+                handles: 'all',
+                target: node
+            });
+
+            me.mon(resizer, 'resizedrag', me.onResizeDrag, me);
+            me.mon(resizer, 'resize', me.onSelectionResize, me);
+
+            me.resizers.push(resizer);
+        }
     },
 
 
@@ -220,6 +263,132 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
     },
 
 
+    clearResizers: function() {
+        var resizers = this.resizers,
+            total = resizers[total];
+        for (var i=0; i < total; i++) {
+            resizers[i].destroy();
+        }
+
+        this.resizers = [];
+    },
+
+
+    /**
+     * get xy coordinates relatively to image el for the case when zoomFactor
+     * equals to 1
+     * @param Ext.dom.Element node
+     * @return array
+     */
+    getSelectionOriginalXY: function(node) {
+        var me = this,
+            zoomFactor = me.zoomFactor,
+            offsetX = me.imgEl.getX(),
+            offsetY = me.imgEl.getY(),
+            xy;
+
+        if (Ext.isArray(node)) {
+            xy = node;
+        } else {
+            xy = node.getXY();
+        }
+        var originalX = Math.round((xy[0] - offsetX) / zoomFactor),
+            originalY = Math.round((xy[1] - offsetY) / zoomFactor);
+
+        return [originalX, originalY];
+    },
+
+
+    /**
+     * get xy coordinates relatively to page for the current zoomFactor
+     * @param array xy
+     * @return array
+     */
+    getSelectionNormalizedXY: function(xy) {
+        var me = this,
+            zoomFactor = me.zoomFactor,
+            offsetX = me.imgEl.getX(),
+            offsetY = me.imgEl.getY(),
+            normalizedX = Math.round(offsetX + xy[0] * zoomFactor),
+            normalizedY = Math.round(offsetY + xy[1] * zoomFactor);
+
+        return [normalizedX, normalizedY];
+    },
+
+
+    /**
+     * get size of selection for the case when zoomFactor
+     * equals to 1
+     * @param Ext.dom.Element node
+     * @return array
+     */
+    getSelectionOriginalSize: function(node) {
+        var me = this,
+            zoomFactor = me.zoomFactor,
+            originalW = Math.round(node.getWidth() / zoomFactor),
+            originalH = Math.round(node.getHeight() / zoomFactor);
+
+        return {width: originalW, height: originalH};
+    },
+
+
+    /**
+     * get size of selection for the current zoomFactor
+     * @param array size
+     * @return array
+     */
+    getSelectionNormalizedSize: function(size) {
+        var me = this,
+            zoomFactor = me.zoomFactor,
+            normW = Math.round(size.width * zoomFactor),
+            normH = Math.round(size.height * zoomFactor);
+
+        return {width: normW, height: normH};
+    },
+
+
+    onResizeDrag: function(resizer, width, height) {
+        var me = this,
+            node = resizer.target,
+            originalXY = me.getSelectionOriginalXY(node),
+            newXY = me.getSelectionNormalizedXY(originalXY),
+            originalSize = me.getSelectionOriginalSize(node),
+            newSize = me.getSelectionNormalizedSize(originalSize);
+
+        node.setXY(newXY);
+        node.setSize(newSize);
+    },
+
+
+    onSelectionResize: function(resizer, width, height) {
+        var me = this,
+            node = resizer.target,
+            record = me.getRecord(node),
+            originalSize = me.getSelectionOriginalSize(node),
+            originalXY = me.getSelectionOriginalXY(node);
+        
+        record.set({
+            'x': originalXY[0],
+            'y': originalXY[1],
+            'w': originalSize.width,
+            'h': originalSize.height
+        });
+
+        me.adjustSelection(record);
+        me.fireEvent('selectionupdate', record);
+    },
+
+
+    onDestroy: function() {
+        var me = this;
+
+        me.clearResizers();
+        me.imgDragger.destroy();
+        me.elDragger.destroy();
+        me.callParent();
+    },
+
+
     onWheel: function(event, direction, x, y) {
         this.zoom(direction, x, y);
     },
@@ -230,6 +399,100 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
 
         // allow dragging only if shiftKey is pressed
         return !!e.shiftKey;
+    },
+
+
+    onBeforeElDrag: function(e) {
+        this.elCursorStartXY = this.elCursorLastXY = e.getXY();
+        if (Ext.fly(e.getTarget()).hasCls(this.selectorClass)) {
+            this.draggedSelection = this.getRecord(e.getTarget());
+            this.dragMode = 'Dragselection';
+        } else {
+            this.dragMode = 'Createselection';
+        }
+    },
+
+
+    onStartElDrag: function(e) {
+        if (this.dragMode == 'Createselection') {
+            var me = this,
+                originalXY = me.getSelectionOriginalXY(me.elCursorStartXY);
+
+            var records = me.store.add([{x: originalXY[0], y: originalXY[1], w: 0, h: 0}]);
+            me.draggedSelection = records[0];
+        }
+    },
+
+
+    onElDrag: function(e) {
+        this['on' + this.dragMode](e);
+    },
+
+
+    onEndElDrag: function(e) {
+        this['on' + this.dragMode + 'End'](e);
+    },
+
+
+    onDragselection: function(e) {
+        var xy = e.getXY(),
+            lastXY = this.elCursorLastXY,
+            zoomFactor = this.zoomFactor,
+            record = this.draggedSelection,
+            offsetX = this.imgEl.getX(),
+            offsetY = this.imgEl.getY(),
+            deltaX = Math.round((xy[0] - lastXY[0]) / zoomFactor),
+            deltaY = Math.round((xy[1] - lastXY[1]) / zoomFactor);
+
+        if (deltaX) {
+            record.set('x', record.get('x') + deltaX);
+        }
+
+        if (deltaY) {
+            record.set('y', record.get('y') + deltaY);
+        }
+
+        if (deltaX || deltaY) {
+            this.adjustSelection(record);
+            this.elCursorLastXY = e.getXY();
+        }
+    },
+
+
+    onDragselectionEnd: function(e) {
+        this.fireEvent('selectionupdate', this.draggedSelection);
+    },
+
+
+    onCreateselection: function(e) {
+        var xy = e.getXY(),
+            startXY = this.elCursorStartXY,
+            x = startXY[0],
+            y = startXY[1],
+            zoomFactor = this.zoomFactor,
+            record = this.draggedSelection,
+            width = xy[0] - startXY[0],
+            height = xy[1] - startXY[1];
+
+        if (width < 0) {
+            x = xy[0];
+        }
+
+        if (height < 0) {
+            y = xy[1];
+        }
+
+        var newWidth = Math.round(Math.abs(width)/zoomFactor),
+            newHeight = Math.round(Math.abs(height)/zoomFactor),
+            newXY = this.getSelectionOriginalXY([x,y]);
+            
+        record.set({x: newXY[0], y: newXY[1], w: newWidth, h: newHeight});
+        this.adjustSelection(record);
+    },
+
+
+    onCreateselectionEnd: function(e) {
+        this.fireEvent('selectioncreate', this.draggedSelection);
     },
 
 
@@ -246,12 +509,12 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
 
 
     onPosChange: function() {
-        console.log('pos', arguments);
+        this.adjustSelections();
     },
 
 
     onSizeChange: function() {
-        console.log('size', arguments);
+        this.adjustSelections();
     },
 
 
@@ -435,6 +698,35 @@ Ext.define('Ext.ux.extjs-imagemapper.ImageMapper', {
     },
 
 
-    adjustSelections: function() {
+    adjustSelections: function(records) {
+        var me = this,
+            record;
+
+        if (!records) {
+            records = me.store.getRange();
+        }
+
+        for (var recordIdx = 0, recordsTotal = records.length;
+            recordIdx < recordsTotal; recordIdx++)
+        {
+            record = records[recordIdx];
+            me.adjustSelection(record);
+        }
+    },
+
+
+    adjustSelection: function(record) {
+        var me = this,
+            offsetX = me.geometry.x || 0,
+            offsetY = me.geometry.y || 0,
+            zoomFactor = me.zoomFactor,
+            node = me.getNode(record),
+            nodeEl = Ext.fly(node);
+
+
+            nodeEl.setLeft(Math.round(offsetX + record.get('x') * zoomFactor));
+            nodeEl.setTop(Math.round(offsetY + record.get('y') * zoomFactor));
+            nodeEl.setWidth(Math.round(record.get('w') * zoomFactor));
+            nodeEl.setHeight(Math.round(record.get('h') * zoomFactor));
     }
 });
